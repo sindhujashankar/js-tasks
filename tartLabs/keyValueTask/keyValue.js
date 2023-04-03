5929d0b60a6a1daa89163a6dc8bc8f53da82b3a0
Sindhuja Shankar <102578378+sindhujashankar@users.noreply.github.com>
2023-04-03 17:47:31 +0530

    Add files via upload

---
+++ tartLabs/keyValueTask/keyValue.js
@@ -0,0 +1,18 @@
+// function myDetails(key,value) {
+//   return {
+//     key:
+//     value:
+//   };
+// }
+
+function info() {
+  key = document.getElementById("key").value;
+  value = document.getElementById("value").value;
+  console.log(key, ":", value);
+  //   console.log(value);
+}
+
+// const details = new Object();
+// details.keyA = document.getElementById("key").value;
+// details.valueB = document.getElementById("value").value;
+// console.log(details.keyA, details.valueB);
